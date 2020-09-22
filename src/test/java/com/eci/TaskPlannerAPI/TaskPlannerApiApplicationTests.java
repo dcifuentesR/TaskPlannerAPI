fea03ef00c9ef9bcfefd24c110a37184ad895c07
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-22 09:05:04 -0500

    first commit

---
+++ src/test/java/com/eci/TaskPlannerAPI/TaskPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.TaskPlannerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
