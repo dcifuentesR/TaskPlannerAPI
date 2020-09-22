66f2389fb5de53de5da5534660672088dee4b5e0
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-22 09:08:57 -0500

    added User and Task POJOs

---
+++ src/main/java/com/eci/TaskPlannerAPI/model/Task.java
@@ -0,0 +1,46 @@
+package com.eci.TaskPlannerAPI.model;
+
+import java.util.Date;
+
+public class Task {
+
+    private String description;
+    private String status;
+    private User responsible;
+    private Date dueDate;
+
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(User responsible) {
+        this.responsible = responsible;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    
+}
